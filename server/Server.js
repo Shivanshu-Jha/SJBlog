b785f5773f26fad3900b702aba1e4f221d036bea
Shivanshu Shekhar Jha <shivanshu1221@gmail.com>
2026-02-10 19:56:24 +0530

    updated

--- server/Server.js
+++ server/Server.js
@@ -8,55 +8,27 @@ import serverless from 'serverless-http';
 
 const app = express();
 
-await connectDB();
 
+
+await connectDB()
+
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.get('/', (req, res) => {
     res.send('API is working');
-});
+})
 
-app.use('/api/admin', adminRouter);
-app.use('/api/blog', blogRouter);
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
 
-// Export handler for Vercel
-export const handler = serverless(app);
+const PORT = process.env.PORT || 3000;
 
-
-
-
-
-
-// import express from 'express';
-// import 'dotenv/config';
-// import cors from 'cors';
-// import connectDB from './configs/db.js';
-// import adminRouter from './routes/adminRoutes.js';
-// import blogRouter from './routes/blogRoutes.js';
-// import serverless from 'serverless-http';
-
-// const app = express();
-
-// await connectDB()
-
-// // Middleware setup
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.get('/', (req, res) => {
-//     res.send('API is working');
-// })
-
-// app.use('/api/admin', adminRouter)
-// app.use('/api/blog', blogRouter )
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
 
 
-// export default app; 
+export default app; 
