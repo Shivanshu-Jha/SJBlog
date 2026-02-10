4227dbe42cf61444d9edc1b3f580d01f13ed6dc3
Shivanshu Shekhar Jha <shivanshu1221@gmail.com>
2026-02-10 19:29:25 +0530

    fixed for deployment

--- server/Server.js
+++ server/Server.js
@@ -4,28 +4,59 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
+import serverless from 'serverless-http';
 
 const app = express();
 
-await connectDB()
+await connectDB();
 
-// Middleware setup
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get('/', (req, res) => {
     res.send('API is working');
-})
+});
 
-app.use('/api/admin', adminRouter)
-app.use('/api/blog', blogRouter )
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+// Export handler for Vercel
+export const handler = serverless(app);
 
 
-export default app; 
+
+
+
+
+// import express from 'express';
+// import 'dotenv/config';
+// import cors from 'cors';
+// import connectDB from './configs/db.js';
+// import adminRouter from './routes/adminRoutes.js';
+// import blogRouter from './routes/blogRoutes.js';
+// import serverless from 'serverless-http';
+
+// const app = express();
+
+// await connectDB()
+
+// // Middleware setup
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.get('/', (req, res) => {
+//     res.send('API is working');
+// })
+
+// app.use('/api/admin', adminRouter)
+// app.use('/api/blog', blogRouter )
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// })
+
+
+// export default app; 
